f859c207b340061567a7baee049963920a2084b3
lanery <ryanilane@gmail.com>
2026-01-07 17:32:11 -0800

    Update __init__.py

--- src/arcadia_microscopy_tools/__init__.py
+++ src/arcadia_microscopy_tools/__init__.py
@@ -1,9 +1,13 @@
 from arcadia_microscopy_tools.channels import Channel
 from arcadia_microscopy_tools.microscopy import MicroscopyImage
+from arcadia_microscopy_tools.pipeline import ImageOperation, Pipeline, PipelineParallelized
 
 __version__ = "0.2.0"
 
 __all__ = [
     "Channel",
     "MicroscopyImage",
+    "ImageOperation",
+    "Pipeline",
+    "PipelineParallelized",
 ]
