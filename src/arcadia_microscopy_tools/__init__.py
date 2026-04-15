fecf3af7cd29757d1d84fe18ef233702883658db
lanery <ryanilane@gmail.com>
2026-04-15 15:43:44 -0700

    Update __init__.py

--- src/arcadia_microscopy_tools/__init__.py
+++ src/arcadia_microscopy_tools/__init__.py
@@ -1,4 +1,4 @@
-from arcadia_microscopy_tools.blending import overlay_channels
+from arcadia_microscopy_tools.blending import BlendMode, Layer, create_overlay, overlay_channels
 from arcadia_microscopy_tools.channels import Channel
 from arcadia_microscopy_tools.exceptions import MetadataWarning, SegmentationWarning
 from arcadia_microscopy_tools.microscopy import MicroscopyImage
@@ -7,11 +7,14 @@ from arcadia_microscopy_tools.pipeline import ImageOperation, Pipeline
 __version__ = "0.3.2"
 
 __all__ = [
+    "BlendMode",
     "Channel",
+    "Layer",
     "MetadataWarning",
     "MicroscopyImage",
     "ImageOperation",
     "Pipeline",
     "SegmentationWarning",
+    "create_overlay",
     "overlay_channels",
 ]
